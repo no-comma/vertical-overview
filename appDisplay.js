c479bb4ad125be5ec0ce9fb6a39e58507f16eb67
no-comma <82644961+no-comma@users.noreply.github.com>
2021-10-15 21:25:08 +0300

    Make the app grid vertical

---
+++ appDisplay.js
@@ -0,0 +1,18 @@
+const { Gio, Meta, Shell, Clutter, GObject, Graphene, St } = imports.gi;
+const Main = imports.ui.main;
+
+function override() {
+    let appDisplay = Main.overview._overview._controls._appDisplay;
+
+    appDisplay._orientation = Clutter.Orientation.VERTICAL;    
+    appDisplay._grid.layoutManager._orientation = Clutter.Orientation.VERTICAL;
+    appDisplay._scrollView.set_policy(St.PolicyType.NEVER, St.PolicyType.EXTERNAL);
+}
+
+function reset() {
+    let appDisplay = Main.overview._overview._controls._appDisplay;
+
+    appDisplay._orientation = Clutter.Orientation.HORIZONTAL;
+    appDisplay._grid.layoutManager._orientation = Clutter.Orientation.HORIZONTAL;
+    appDisplay._scrollView.set_policy(St.PolicyType.EXTERNAL, St.PolicyType.NEVER);
+}
